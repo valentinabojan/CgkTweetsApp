0307d79936186c5d1d21eca775f7f0fab0b510e8
valentinabojan <valentina.bojan@cti.pub.ro>
2016-02-19 11:08:57 +0200

    Remove some bad paths in scripts

--- src/main/resources/scripts/cassandra-create.sh
+++ src/main/resources/scripts/cassandra-create.sh
@@ -1,2 +1,2 @@
-/opt/dsc-cassandra-2.2.5/bin/cqlsh -f ~/Desktop/Mongo\ DB/tweets/src/main/resources/cassandra/tweet.cql
-/opt/dsc-cassandra-2.2.5/bin/cqlsh -f ~/Desktop/Mongo\ DB/tweets/src/main/resources/cassandra/comment.cql
+/opt/dsc-cassandra-2.2.5/bin/cqlsh -f ../cassandra/tweet.cql
+/opt/dsc-cassandra-2.2.5/bin/cqlsh -f ../cassandra/comment.cql
